fc6d8a014b1716d81c996ee873076d34312a1580
adrianhprogramming <adrianhasudungan.hutabarat@student.adelaide.edu.au>
2025-03-04 22:32:36 +1030

    I have added the count and main programs.

---
+++ count.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int count(int array[], int n){
+    int counter = 0;
+    for (int i = 0; i < n; i++)
+        if (array[i] > 0){
+        counter++;
+    }
+    
+    
+        /* code */
+    
+    return counter;
+}
+    
+
+
