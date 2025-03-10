a2ff2ab76feabc065a672e643c96730df20d4642
adrianhprogramming <adrianhasudungan.hutabarat@student.adelaide.edu.au>
2025-03-10 21:52:52 +1030

    made files compile

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -5,14 +5,6 @@
 
 bool is_ascending(int array[], int n);
 
-int main(){
-    int array[] = {2};
-    int n = sizeof(array)/sizeof(array[0]);
-    std::cout << std::boolalpha;
-    std::cout << is_ascending(array, n)  << std::endl;
-    return 0;
-}
-
 bool is_ascending(int array[], int n){
     bool ascending = true;
     int i;
