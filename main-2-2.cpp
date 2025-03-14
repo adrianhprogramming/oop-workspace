119d5f95622aac6d4219771b14b5eef4b74f4614
adrianhprogramming <adrianhasudungan.hutabarat@student.adelaide.edu.au>
2025-03-14 14:31:41 +1030

    wrong function name

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -7,5 +7,5 @@ extern int binary_to_int(int binary_digits[], int number_of_digits);
 int main(){
 int binary_digits[] = { 1,0,0,1,1,1,0};
     int number_of_digits = sizeof(binary_digits)/sizeof(binary_digits[0]);
-    std::cout << binary_to_init (binary_digits, number_of_digits);
+    std::cout << binary_to_int(binary_digits, number_of_digits);
 }   
