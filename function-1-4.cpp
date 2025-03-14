f7e39434e6c6358fbed48a5eac22b114ae12f7a8
adrianhprogramming <adrianhasudungan.hutabarat@student.adelaide.edu.au>
2025-03-14 23:23:41 +1030

    Q1-4 and Q1-5

---
+++ function-1-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+void print_scaled(int array[3][3],int scale);
+
+void print_scaled(int array[3][3],int scale){
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+        std::cout << array[i][j] * scale << " ";
+        
+    }
+    std::cout <<  "\n";
+}
+
+}
